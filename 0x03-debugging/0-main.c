fd566afdc725b227618acffa26c21a2063118ed9
jayse45 <ngyasi0@gmail.com>
2021-06-24 15:02:57 +0000

    correct

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
+
 /**
- * main - tests functions that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -10,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
