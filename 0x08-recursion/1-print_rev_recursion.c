10b9a984903ea9a034577e39ccb1b8a9d0b28223
jayse45 <ngyasi0@gmail.com>
2021-07-07 20:04:57 +0000

    correction

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
-
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
+ * _print_rev_recursion - print a string in reverse
+ * @s: string character
+ * Return: no return
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
