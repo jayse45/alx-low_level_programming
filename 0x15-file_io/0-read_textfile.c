e827e8d0e9d26cf2b9e93e32d4b1c7a7bf3f3348
jayse45 <ngyasi0@gmail.com>
2021-08-09 19:43:54 +0000

    #define MAXSIZE 1204

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,31 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile -function that reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: pointer
+ * @letters: is the number of letters it should read and print
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: the actula number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
+	ssize_t bytes_read;
 	char *buf;
+	int fd;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
+	buf = malloc(letters * sizeof(char));
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	bytes_read = read(fd, buf, letters);
+	bytes_read = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_read == -1)
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
 	close(fd);
 
-	free(buf);
-
-	return (nwr);
+	return (bytes_read);
 }
